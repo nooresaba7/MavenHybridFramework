4aa69f9b3268cec2bc099131a27ea66ad969e0f9
noor <noor@jujkikio.qedge.com>
2019-12-26 10:33:29 +0530

    Maven Hybrid Framework

---
+++ src/main/java/Utilities/PropertyFileUtil.java
@@ -0,0 +1,14 @@
+package Utilities;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class PropertyFileUtil {
+public static String getValueForKey(String key)throws Throwable
+{
+	Properties configProperties=new Properties();
+	FileInputStream fi=new FileInputStream("D:\\Selenium_framework\\ERP_Maven\\PropertyFile\\Enviroment.properties");
+	configProperties.load(fi);
+	return configProperties.getProperty(key);
+}
+}
